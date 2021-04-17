7ed205f33d32aa14637603f3d0d356b8df4cdb84
msenat7432 <maxsenat7432@gmail.com>
2021-04-17 11:18:41 -0400

    move folder

---
+++ FrontEnd/src/components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+			<div>
+    <form className='create-form'>
+	<label for=''> </label>
+	<input name='' placeholder='' />
+	<label for=''> </label>
+	<input name=''placeholder=''/>
+	<button id='button' type='submit'>Submit</button>
+	</form>
+			</div>
+		);
+};
+
+export default Form;
