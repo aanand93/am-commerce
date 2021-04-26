16be39d51713962f1960efa6bb51e1b8837ed310
aanand93 <alex329@mac.com>
2021-04-25 23:39:16 -0400

    Added styling to apparel

--- src/components/Apparel/Apparel.js
+++ src/components/Apparel/Apparel.js
@@ -1,54 +1,105 @@
 import React from 'react';
 import './Apparel.css';
-import { Link } from 'react-router-dom';
-import Apparelitem from './Apparelitem'
 
-function Apparel({apparel}) {
-    
-    return (
-			<div className='card'>
-				<h1>Check out our EPIC Apparel!</h1>
-				<div className='cards__container'>
-					<div className='cards__wrapper'>
-						<ul className='cards__items'>
-							<Apparelitem
-								src='images/'
-								text='One of our amazing shirts'
-								label='Shirts'
-								path='/form'
-							/>
-							<Apparelitem
-								src=''
-								text='Army Fatigue Hoodie'
-								label='SweatShirt'
-								path='/form'
-							/>
-						</ul>
-						<ul className='cards__items'>
-							<Apparelitem
-								src=''
-								text='Dad Hat'
-								label='Hat'
-								path='/form'
-							/>
-							<Apparelitem
-								src=''
-								text='Shirt'
-								label='Adventure'
-								path='/form'
-							/>
-							<Apparelitem
-								src=''
-								text=''
-								label=''
-								path='/sign-up' 
-							/>
-						</ul>
+function Apparel() {
+	return (
+		<div className='Apparel3'>
+			<h1>Apparel</h1>
+			<h4>Here is some of the hottest styles that we offer</h4>
+			<div className='apparel-container'>
+				<div className='container2'>
+					<div className='card'>
+						<h5>Next Level CVC T-Shirt</h5>
+						<img
+							src='https://cdn.ssactivewear.com/Images/Style/3227_fm.jpg?v=2'
+							alt='tshirts'
+							className='apparel-image'
+						/>
+					</div>
+				</div>
+				<div className='container2'>
+					<div className='card'>
+						<h5>Bella Canvas CVC T-Shirt</h5>
+						<img
+							src='https://cdn.ssactivewear.com/Images/Style/7584_fm.jpg?v=2'
+							alt='tshirts'
+							className='apparel-image'
+						/>
+					</div>
+				</div>
+				<div className='container2'>
+					<div className='card'>
+						<h5>Bella Canvas CVC T-Shirt</h5>
+						<img
+							src='https://cdn.ssactivewear.com/Images/Style/16_fm.jpg?v=2'
+							alt='tshirts'
+							className='apparel-image'
+						/>
+					</div>
+				</div>
+				<div className='container2'>
+					<div className='card'>
+						<h5>Richardson Adjustable Snapback</h5>
+						<img
+							src='https://cdn.ssactivewear.com/Images/Style/4332_fm.jpg?v=2'
+							alt='hats'
+							className='apparel-image'
+						/>
+					</div>
+				</div>
+				<div className='container2'>
+					<div className='card'>
+						<h5>47 Brand Clean Up Cap 4700</h5>
+						<img
+							src='https://cdn.ssactivewear.com/Images/Style/9182_fm.jpg?v=2'
+							alt='hats'
+							className='apparel-image'
+						/>
+					</div>
+				</div>
+				<div className='container2'>
+					<div className='card'>
+						<h5>Independent Trading Co. Sweatshirt ss4500</h5>
+						<img
+							src='https://cdn.ssactivewear.com/Images/Style/1828_fm.jpg?v=2'
+							alt='hoodies'
+							className='apparel-image'
+						/>
+					</div>
+				</div>
+				<div className='container2'>
+					<div className='card'>
+						<h5>Independent Trading Co. Midnight Fleece Shorts</h5>
+						<img
+							src='https://cdn.ssactivewear.com/Images/Style/8608_fm.jpg?v=2'
+							alt='shorts'
+							className='apparel-image'
+						/>
+					</div>
+				</div>
+				<div className='container2'>
+					<div className='card'>
+						<h5>Independent Trading Co. Lightweight jacket</h5>
+						<img
+							src='https://cdn.ssactivewear.com/Images/Style/5996_fm.jpg?v=2'
+							alt='shorts'
+							className='apparel-image'
+						/>
+					</div>
+				</div>
+				<div className='container2'>
+					<div className='card'>
+						<h5>Independent Trading Co. Joggers</h5>
+						<img
+							src='https://cdn.ssactivewear.com/Images/Style/8607_fm.jpg?v=2'
+							alt='shorts'
+							className='apparel-image'
+						/>
 					</div>
 				</div>
 			</div>
-		);
+		</div>
+	);
 }
-   
 
 export default Apparel;
